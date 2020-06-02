fab66a80652840642c611708a346f39031419806
Wesley Nguyen <nguyenw@seattleu.edu>
2020-06-02 10:01:39 -0700

    add directories

---
+++ src/helpers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Import Modules
+import numpy as np
+import pandas as pd
+from pprint import pprint
+import json
+import requests
+import re
+from typing import List
+
+
+'''PARSING'''
+def re_match_target(trg:str, to_match:str) -> bool:
+    """ Returns bool if to_match pattern string in target, set to_match var for another match """
+    pattern  = re.compile(f'{to_match}', re.I)
+    return bool(pattern.search(trg))
