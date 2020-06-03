8538772f671c885d695f40ca7f498086cda66592
Wesley Nguyen <nguyenw@seattleu.edu>
2020-06-02 18:24:22 -0700

    config selenium

---
+++ src/scrape_functions.py
@@ -0,0 +1,34 @@
+# Import Modules
+import numpy as np
+import copy
+import pandas as pd
+import json
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import re
+import os
+import time
+
+
+# Function for statically scraping item brand, name, # loves without needing selenium
+def sephora_parse_item(item_url:str) -> tuple:
+    '''Returns item/product brand, name, # loves from a given sephora url'''
+    # GET Request to URL
+    page = requests.get(item_url)
+    # Create BS4 Object
+    html = BeautifulSoup(page.text, "html.parser")
+    
+    # Find Name and Brand
+    brand, name = (x.get_text() for x in html.find("h1", attrs={"data-comp":True}))
+    
+    # Find n_loves (product likes)
+    n_loves = html.find('span', attrs={"data-at":"product_love_count"}).get_text()
+    
+    return brand, name, n_loves
+
+
