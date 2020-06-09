c8bac2af8727a32e5ae4523363a06d3b263bf706
Wesley Nguyen <nguyenw@seattleu.edu>
2020-06-09 14:07:42 -0700

    visualize ingredient effects

--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,7 @@
 
 # Import Modules
 from datetime import datetime, timedelta
+from collections import Counter
 import numpy as np
 import pandas as pd
 from pprint import pprint
@@ -9,6 +10,53 @@ import json
 import requests
 import re
 from typing import List
+
+
+ingredient_alias = ['acrylates/c10-30 alkyl acrylate crosspolymer',
+ 'ammonium acryloyldimethyltaurate/vp copolymer',
+ 'bacillus/soybean/ folic acid ferment extract',
+ 'caprylic / capric triglyceride',
+ 'caprylic/capric triglyceride',
+ 'coco-caprylate/caprate',
+ 'dimethicone/peg-10/15 crosspolymer',
+ 'dimethicone/phenyl vinyl dimethicone crosspolymer',
+ 'epilobium angustifolium flower/leaf/stem extract',
+ "hordeum vulgare (barley) extract/extrait d'orge",
+ 'hydroxyethyl acrylate/sodium acryloyldimethyl taurate copolymer',
+ 'lactic acid/glycolic acid copolymer',
+ 'leuconostoc / radish root ferment filtrate',
+ 'myrothamnus flabellifolia leaf/stem extract',
+ 'ocimum basilicum (basil) flower/leaf extract',
+ 'parfum/fragrance',
+ 'peg/ppg/polybutylene glycol-8/5/3 glycerin',
+ 'polyglyceryl-4 diisostearate/polyhydroxystearate/sebacate',
+ 'saccharomyces/camellia sinensis leaf/cladosiphon okamuranus/rice ferment filtrate*',
+ 'sodium acrylate/acryloyldimethyltaurate/dimethylacrylamide crosspolymer']
+
+
+ingredient_remap = [
+    'acrylates',
+    'ammonium acryloyldimethyltaurate',
+    'bacillus/soybean',
+    'capric triglyceride',
+    'capric triglyceride',
+    'coco-caprylate',
+    'dimethicone',
+    'dimethicone',
+    'epilobium angustifolium flower',
+    "hordeum vulgare (barley) extract",
+    'hydroxyethyl acrylate',
+    'lactic acid',
+    'leuconostoc',
+    'myrothamnus flabellifolia leaf',
+    'ocimum basilicum (basil) flower',
+    'parfum/fragrance',
+    'polybutylene glycol-8',
+    'polyhydroxystearate',
+    'saccharomyces',
+    'sodium acrylate'
+]
+
 
 
 '''PARSING'''
@@ -82,10 +130,33 @@ def parse_review_string(review:list) -> str:
 def clean_ingredients(ingredients:list) -> list:
     '''Returns cleaned ingredients list from given one, cleaned in accordance to custom rules'''
     # Copy list to avoid collision
-    tmp = ingredients.copy()
+    tmp, remap = ingredients.copy(), dict(zip(ingredient_alias, ingredient_remap))
     # Standardize water as first ingredient
     water_match = lambda x: re_match_target(x, to_match='water')
     tmp[0] = 'water' if water_match(tmp[0]) else tmp[0]
-    
+    for i in range(len(tmp)):
+        cur = tmp[i]
+        res = remap.get(cur, None)
+        if res:
+            tmp[i] = res
     return sorted(tmp)
     
+
+def get_effect_counts(ingredients:list, chem_df:pd.DataFrame) -> dict:
+    '''Returns counter object of all counts of each effect present in given item'''
+    effects = ['moisture', 'antioxidant', 'soothing', 'irritancy', 'brightening', 'viscosity', 'cleaning', 'fragrance']
+    remap = dict(zip(effects, [0 for _ in range(len(effects))]))
+    counts = []
+    for i in ingredients:
+        res = chem_df[chem_df['chemicals'] == i]['feature'] 
+        if not res.empty:
+            counts += res.tolist()
+    return dict(remap, **Counter(counts))
+
+
+def set_map_effects(df:pd.DataFrame, chem_df:pd.DataFrame) -> None:
+    for idx in range(df.shape[0]):
+        effect_dict = get_effect_counts(df.iloc[idx]['ingredients'], chem_df)
+        for key, val in effect_dict.items():
+            df.at[idx, key] = val
+    return
